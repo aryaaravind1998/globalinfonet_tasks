8347325f458c95878815c6ac969f645f019b68ca
Arya <aryamolca@gmail.com>
2021-05-26 14:27:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const date = new Date();
+
+const renderCalendar = () => {
+    date.setDate(1);
+
+    const monthDays = document.querySelector(".days");
+    const lastDay = new Date(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        0
+      ).getDate()
